097504e2a3973779f34a44410d81534a49de6962
Megan Steenkamp <megan.steenkamp@centrapay.com>
2022-02-03 17:29:33 +1300

    Remove redundant references to legacy

--- kete-server/src/index.plugin.js
+++ kete-server/src/index.plugin.js
@@ -3,14 +3,6 @@
 const fs = require('fs').promises;
 const template = require('./script.html.template.js');
 
-async function getLegacyIndex(req, h) {
-  const rawFile = await fs.readFile(`${__dirname}/../legacy-static/legacy-index.html`, 'utf-8');
-  const split = rawFile.split('<head>');
-  const indexHtml = `${split[0]}<head>${template()}${split[1]}`;
-
-  return h.response(indexHtml).type('text/html').code(200);
-}
-
 async function getIndex(req, h) {
   const rawFile = await fs.readFile(`${__dirname}/../static/index.html`, 'utf-8');
   const split = rawFile.split('<head>');
@@ -27,15 +19,6 @@ function register(hapiServer) {
       auth: false
     },
     handler: getIndex,
-  });
-
-  hapiServer.route({
-    method: 'GET',
-    path: '/v1/{path*}',
-    options: {
-      auth: false
-    },
-    handler: getLegacyIndex,
   });
 }
 
